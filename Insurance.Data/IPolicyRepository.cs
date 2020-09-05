5eac9b0de09fc3ea257e38c06ae94d8e93cb9a62
deagosb <deagosb@gmail.com>
2020-09-05 18:57:36 -0500

    Add Repository

---
+++ Insurance.Data/IPolicyRepository.cs
@@ -0,0 +1,47 @@
+﻿using Insurance.Domain;
+using System.Collections.Generic;
+
+namespace Insurance.Data
+{
+    /// <summary>
+    /// Defines the <see cref="IPolicyRepository" />.
+    /// </summary>
+    public interface IPolicyRepository
+    {
+        /// <summary>
+        /// The GetAll.
+        /// </summary>
+        /// <returns>The <see cref="IEnumerable{Policy}"/>.</returns>
+        IEnumerable<Policy> GetAll();
+
+        /// <summary>
+        /// The GetById.
+        /// </summary>
+        /// <param name="policyId">The policyId<see cref="long"/>.</param>
+        /// <returns>The <see cref="Policy"/>.</returns>
+        Policy GetById(long policyId);
+
+        /// <summary>
+        /// The Insert.
+        /// </summary>
+        /// <param name="policy">The policy<see cref="Policy"/>.</param>
+        void Insert(Policy policy);
+
+        /// <summary>
+        /// The Update.
+        /// </summary>
+        /// <param name="policy">The policy<see cref="Policy"/>.</param>
+        void Update(Policy policy);
+
+        /// <summary>
+        /// The Delete.
+        /// </summary>
+        /// <param name="policyId">The policyId<see cref="long"/>.</param>
+        void Delete(long policyId);
+
+        /// <summary>
+        /// The Save.
+        /// </summary>
+        void Save();
+    }
+}
